2c2828a7d95cb1c0f913f08ba29b274660d071e2
Dev-Laptop <info@fll-fluffys.ch>
2025-04-23 18:56:25 +0200

    .

--- run2.py
+++ run2.py
@@ -6,42 +6,43 @@ from wall import wall
 
 def run2():
 
+
     wall.rightTo(-20)
     wait(100)
     wall.rightTo(7)
     drive.straight_ms(500, -300)
     wait(50)
     wall.upTo(5)
-    drive.turn_to_and_drive(0, 670)
+    drive.drive_to(0, 670)
     wall.rightTo(-20, wait=False)
     drive.rotate_forward(-89)
     wall.rightTo(-35)
 
     # Anglerfisch 
-    drive.turn_to_and_drive(-89, 645)
+    drive.drive_to(-89, 645)
     wall.rightTo(-20)
 
     # Bodenprobe aufnehmen
-    drive.turn_to_and_drive(-90, -43)
+    drive.drive_to(-90, -43)
     wall.rightTo(75)
-    drive.turn_to_and_drive(-90, 170)
+    drive.drive_to(-90, 170)
     
     # wait_for_button_pressed()
 
     # Wasserprobe wegschieben 
     wall.rightTo(30)
-    drive.turn_to_and_drive(-90, 190)####
+    drive.drive_to(-90, 190)####
 
     wall.rightTo(-55, wait=False)
 
-    drive.turn_to_and_drive(-90, 90)
+    drive.drive_to(-90, 90)
 
     wall.rightTo(-75)
     wall.rightTo(-55, wait=False)
     wall.upTo(30)
 
     # await gyro_fahren(3.8, 400, 90)
-    drive.turn_to_and_drive(-90, 38)
+    drive.drive_to(-90, 38)
     
     wall.rightTo(-45)
     wall.upTo(5)
@@ -50,7 +51,7 @@ def run2():
     wall.rightTo(45, wait=False)
 
     # await gyro_fahren(17, 1050, 88)#
-    drive.turn_to_and_drive(-88,170)
+    drive.drive_to(-88,170)
     
     wait(100)
 
@@ -62,7 +63,7 @@ def run2():
     wall.upTo(10)
     
     # await gyro_fahren(-20, 1050, 90)
-    drive.turn_to_and_drive(-90,-200)
+    drive.drive_to(-90,-200)
 
     wall.upTo(0, wait=False)
     wall.rightTo(0, wait=False)
@@ -73,12 +74,12 @@ def run2():
     wall.rightTo(30)
 
     # await gyro_fahren(13, 1050, 140)
-    drive.turn_to_and_drive(-140,130)
+    drive.drive_to(-140,130)
     
     wall.rightTo(0)
 
     # await gyro_fahren(57, 1050, 140)
-    drive.turn_to_and_drive(-140,570)
+    drive.drive_to(-140,570)
 
 
 
--- run4.py
+++ run4.py
@@ -12,22 +12,22 @@ def run4():
 
     wall.rightTo(-20)
     wall.rightTo(20)
-    drive.turn_to_and_drive(0, 555)
+    drive.drive_to(0, 555)
     wait(50)
     wall.rightTo(30, wait=False)
     drive.rotate_to_forward(-49)
 
     wall.upTo(120)
-    drive.turn_to_and_drive(-50, 78)
+    drive.drive_to(-50, 78)
 
     wall.upTo(35)
     drive.straight_ms(200, 200)
     wall.upTo(0)
     wait(200)
     wall.upTo(110)
-    drive.turn_to_and_drive(-50, -64)
+    drive.drive_to(-50, -64)
     wall.upTo(43, wait=False)
     drive.rotate_backward(0)
     wall.rightTo(50, wait=False)
-    drive.turn_to_and_drive(0, -610)
+    drive.drive_to(0, -610)
     
