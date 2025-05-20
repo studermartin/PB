26af71b4af008e2b75f588df330adc76beced14b
martin.studer <martin.studer@gkd.ch>
2025-05-20 13:42:09 +0200

    Closes #26

--- hub.py
+++ hub.py
@@ -1,14 +1,18 @@
 from pybricks.hubs import PrimeHub
-from pybricks.parameters import Axis
+from pybricks.pupdevices import ForceSensor
+from pybricks.parameters import Axis, Port
 from pybricks.tools import wait
 
 hub = PrimeHub(top_side=Axis.Z, front_side=Axis.Y)
 
+_force_sensor = ForceSensor(Port.F)
+
 def wait_for_button_pressed():
     pressed = []
-    while not any(pressed):
+    while not any(pressed) and not _force_sensor.pressed():
         pressed = hub.buttons.pressed()
         wait(100)
+    beepHigh()
 
 def beepLow():
     hub.speaker.beep(440, 100)
---
+++ test_hub.py
@@ -0,0 +1,6 @@
+from hub import *
+
+wait_for_button_pressed()
+
+
+
