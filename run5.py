ab372dd176ecdf4e6af8722d595a83a921941b52
Dev-Laptop <info@fll-fluffys.ch>
2025-06-02 21:20:43 +0200

    .

--- run5.py
+++ run5.py
@@ -8,16 +8,16 @@ def run5():
     wall.rightTo(62)#nur bei run 5 einzeln
     wait_for_button_pressed()#nur bei run 5 einzeln
 
-    drive.drive_to(940,0)
-    wait(100)
-    #wait_for_button_pressed()
-    drive.drive_to(-140, 0)
+
+    drive.straight(960,straight_acceleration=(30))
+    wait_for_button_pressed()
+    drive.straight(-160,straight_acceleration=100)
     #wait_for_button_pressed()
     wall.upTo(90)
     #wait_for_button_pressed()
-    drive.drive_to(-260, 0, speed=500, straight_acceleration=9700)
-    wait_for_button_pressed()
-    wall.rightTo(0, wait=False)
+    drive.drive_to(-260, straight_acceleration=(9775))
+    #wait_for_button_pressed()
+    #wall.rightTo(0, wait=False)
     drive.rotate_to_forward(-14)
     drive.drive_to(425, -14)
     drive.rotate_to_forward(15)
