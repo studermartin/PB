4a200d49add59c6e3b9796c013d93d3f36133ef1
Dev-Laptop <info@fll-fluffys.ch>
2025-06-23 15:58:53 +0200

    .

--- run5.py
+++ run5.py
@@ -4,26 +4,28 @@ from hub import wait_for_button_pressed
 from wall import wall
 
 def run5():
-    wall.upTo(40)#nur bei run 5 einzeln
-    wall.rightTo(60)#nur bei run 5 einzeln
-    wait_for_button_pressed()#nur bei run 5 einzeln
+    #wall.upTo(40)#nur bei run 5 einzeln
+    #wall.rightTo(60)#nur bei run 5 einzeln
+    #wait_for_button_pressed()#nur bei run 5 einzeln
 
 
     drive.drive_to(943, 0)
     drive.drive_to(-150, 0, straight_acceleration=100)
     wall.upTo(110)
-    drive.drive_to(-230, 0, speed=450, straight_acceleration=9775)
+    drive.drive_to(-230, 0, speed=350,)
     #wait_for_button_pressed()
     #wall.rightTo(0, wait=False)
     drive.rotate_to_forward(-14)
     drive.drive_to(325, -14)
-    wall.upTo(70)
-    drive.drive_to(-80, -14)
-    drive.drive_to(180, -14)
+    wall.upTo(65)
+    drive.drive_to(-120, -14)
+    drive.drive_to(220, -14)
     drive.rotate_to_forward(15)
     wall.upTo(80, wait=False)
+    wall.rightTo(0, wait=False)
+    wall.upTo(40, wait=False)
     drive.drive_to(750, 15, speed=500)
-    wall.upTo(30)
+    
     drive.stop()
 
     
--- run6.py
+++ run6.py
@@ -4,17 +4,17 @@ from hub import wait_for_button_pressed
 from wall import wall
 
 def run6():
-    wall.upTo(30, speed=200)#nur bei run 6 alleine
-    wait_for_button_pressed()#nur bei run 6 alleine
+    #wall.upTo(40, speed=200)#nur bei run 6 alleine
+    #wait_for_button_pressed()#nur bei run 6 alleine
 
-    wall.upTo(28)
+    wall.upTo(38)
     wall.upTo(100, wait=False)
     drive.drive_to(725, 0)
     drive.rotate_to_forward(44)
     wall.upTo(40, speed=400)
     drive.drive_to(100, 44, speed=200)
     drive.straight_ms(170, speed=120)
-    wait(200)
+    wait(500)
     #wait_for_button_pressed()
     drive.drive_to(-90, 43, speed=100)
     #wait_for_button_pressed()
@@ -28,11 +28,11 @@ def run6():
     wall.rightTo(60)
     wall.upTo(45)
     wall.upTo(100, 10000)
-    wall.rightTo(70)
+    wall.rightTo(78)
     wall.rightTo(40)
     #wait_for_button_pressed()
     drive.rotate_to_backward(0)
-    drive.drive_to(10, 0)
+    drive.drive_to(5, 0)
     wall.rightTo(65, wait=False)
     drive.rotate_to_forward(-90)
     wall.upTo(40, speed=300, wait=False)
@@ -43,11 +43,12 @@ def run6():
     drive.drive_to(65, -90)
     wall.rightTo(70, speed=300)
     wall.upTo(100)
-    drive.drive_to(150, -90)
-    wall.rightTo(75, wait=False)
-    wall.upTo(18, speed=400, wait=False)
-    drive.drive_to(205, -90, speed=200)
-    wall.upTo(80)
+    drive.straight(150)
+    wall.rightTo(78, wait=False)
+    wall.upTo(18, speed=400)
+    drive.rotate_to_forward(-85)
+    drive.straight(205)
+    wall.upTo(80, speed=200)
     wait(1000)
     wall.rightTo(0, speed=400)
 
